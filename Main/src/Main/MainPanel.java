5112f0cec2f491ab178859c8a1018b1f2a3660dd
mkelley <xkelley0529@gmail.com>
2019-12-24 04:49:16 -0600

    Commit

--- Main/src/Main/MainPanel.java
+++ Main/src/Main/MainPanel.java
@@ -8,8 +8,10 @@ import java.awt.event.ActionListener;
 public class MainPanel extends JFrame implements ActionListener {
     //Final Variables
     private final static String TITLE="MTGPiApp", VERID="0.1";
+    private final int MAXPLAYERCOUNT=8, MINPLAYERCOUNT=2;
     private final int COMMANDER=0, CONSTRUCTED=1, TWOHEADED=2;
     //UI Variables
+    private JPanel pane=new JPanel();
     private MenuBar menuBar=new MenuBar();
     private String[] menuTitles={"Options","Help"};
     private Menu[] menus=new Menu[menuTitles.length];
@@ -23,17 +25,11 @@ public class MainPanel extends JFrame implements ActionListener {
         setTitle(TITLE+" "+VERID);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
-        JPanel pane=new JPanel();
-        pC=playerCount;
-        PlayerPanel players[]=new PlayerPanel[pC];
-        pane.setLayout(new GridLayout((int)Math.round(pC/2.),pC/2,5,5));
         for(int i=0;i<fgColors.length;i++) {
             fgColors[i]=Color.BLACK;
         }
-        for(int i=0;i<pC;i++) {
-            players[i]=new PlayerPanel(fgColors[i], bgColors[i], getStartingLifeTotal());
-            pane.add(players[i]);
-        }
+        pC=playerCount;
+        setupPlayers();
         for(int i=0;i<menus.length;i++) {
             menus[i]=new Menu(menuTitles[i]);
             menuBar.add(menus[i]);
@@ -44,18 +40,50 @@ public class MainPanel extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent evt) {
 
     }
+    private void setupPlayers() {
+        PlayerPanel players[]=new PlayerPanel[pC];
+        pane.setLayout(new GridLayout((int)Math.round(pC/2.),pC/2,5,5));
+        for(int i=0;i<pC;i++) {
+            players[i]=new PlayerPanel(fgColors[i], bgColors[i], getStartingLifeTotal());
+            pane.add(players[i]);
+        }
+    }
+    private void setFormat(int format) {
+        if(!(format<0||format>2)) {
+            this.format=format;
+        }
+        setPlayerCount(getDefaultPlayerCount());
+        setupPlayers();
+    }
+    private void setPlayerCount(int playerCount) {
+        if(!(playerCount<MINPLAYERCOUNT||playerCount>MAXPLAYERCOUNT)) {
+            pC=playerCount;
+        }
+    }
     private int getStartingLifeTotal() {
-        if(format==COMMANDER)
-            return 40;
-        else if(format==CONSTRUCTED)
-            return 20;
-        else if(format==TWOHEADED)
-            return 30;
-        else
-            return -1;
+        switch (format) {
+            case COMMANDER:
+                return 40;
+            case CONSTRUCTED:
+                return 20;
+            case TWOHEADED:
+                return 30;
+            default:
+                return -1;
+        }
+    }
+    private int getDefaultPlayerCount() {
+        switch (format) {
+            case COMMANDER:
+                return 4;
+            case CONSTRUCTED:
+            case TWOHEADED:
+                return 2;
+            default:
+                return -1;
+        }
     }
     public static void main(String[] args) {
-        JFrame frame=new JFrame();
         MainPanel mainPanel=new MainPanel(pC);
         mainPanel.setVisible(true);
     }
